41f0af571c58378d2ba806cedfe6fe4615d63959
bart988 <kidacki988@gmail.com>
2021-09-05 20:41:59 -0400

    Resume Website rough draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
+import {MainPageComponent} from './main-page/main-page.component';
+import {RouterModule} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
+import {AboutComponent} from './about/about.component';
+import {EducationComponent} from './education/education.component';
+import {SkillsComponent} from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import {HttpClientModule} from "@angular/common/http";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatCardModule} from "@angular/material/card";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainPageComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+
+
   ],
   imports: [
+    RouterModule.forRoot([
+
+        {path: '', component: MainPageComponent},
+      ],
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatCardModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
+    matIconRegistry.addSvgIconSet(
+      domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg')
+    );
+  }
+}
